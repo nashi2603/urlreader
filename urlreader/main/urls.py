f98e9bea27a3d3ead0af45fff0163b3ee3813a35
nashi2603 <nashi.20306.y@gmail.com>
2019-10-31 11:25:38 +0900

    ディレクトリ構造を作り変え

--- urlreader/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('/top', views.index, name='top'),
-    path('/results', views.results, name='results'),
-    path('/guides', views.guides, name='guides')
-]
