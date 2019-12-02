5395ef3d13f1881e6b86f4c4f4aa58e7a0b0f598
nashi2603 <nashi.20306.y@gmail.com>
2019-12-02 14:52:08 +0900

    ひとまずfile upload完全に理解した

--- urlreader/top/forms.py
+++ urlreader/top/forms.py
@@ -1,10 +1,5 @@
 from django import forms
-from django.core.files.storage import default_storage
 
-class UploadForm(forms.Form):
-    file = forms.ImageField(label='imagefile')
-
-    def save(self):
-        upload_file = self.cleaned_data['file']
-        file_name = default_storage.save(upload_file.name, upload_file)
-        return default_storage.url(file_name)
+class UploadFileForm(forms.Form):
+    # title = forms.CharField(max_length=50)
+    file = forms.FileField()
--- urlreader/top/views.py
+++ urlreader/top/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from .forms import UploadFileForm
+from django.conf import settings
 
 # Create your views here.
 
 def top(request):
-    x_value = request.GET.get(key='x', default='0')
-    y_value = request.GET.get(key='y', default='0')
-    numbers = int(x_value) + int(y_value)
-    params = {
-        'numbers': numbers,
-    }
-    return render(request, 'top.html', params)
+    # x_value = request.GET.get(key='x', default='0')
+    # y_value = request.GET.get(key='y', default='0')
+    # numbers = int(x_value) + int(y_value)
+    # params = {
+    #     'numbers': numbers,
+    # }
+    print(request.FILES)
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            Handle_Uploaded_File(request.FILES['file'])
+            return HttpResponseRedirect('/results')
+    else:
+        form = UploadFileForm()
+    return render(request, 'top.html', {'form': form})
+
+def Handle_Uploaded_File(f):
+    BASE_DIR = getattr(settings, "BASE_DIR", None)
+    with open(BASE_DIR + "\\images\\" + f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
